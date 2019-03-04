d746506b5e74a24760d9453bc00b743bd733ea88
Evelin <evelin992009@gmail.com>
2019-03-04 14:44:35 -0500

    FUNCIONAN LOS FRAGMENTOS

---
+++ app/src/main/java/com/example/familia/ejemplofragments/HomeFragment.java
@@ -0,0 +1,62 @@
+package com.example.familia.ejemplofragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+       button1=view.findViewById(R.id.b1);
+       button2=view.findViewById(R.id.b4);
+       button3=view.findViewById(R.id.b5);
+       button1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new FirstFragment(),null).commit();
+               //MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new secondFragment(),null).commit();
+           }
+
+       }) ;
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new secondFragment(),null).commit();
+                //MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new secondFragment(),null).commit();
+            }
+
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new ThirdFragment(),null).commit();
+                //MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new secondFragment(),null).commit();
+            }
+
+        });
+        return view;
+    }
+
+}
